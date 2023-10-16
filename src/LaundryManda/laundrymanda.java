89544c51494f36064b1a13fb61e8c06f34f4c16f
MyName269 <amandaanggraini287@gmail.com>
2023-10-16 15:02:48 +0700

    Aplikasi Desktop

---
+++ src/LaundryManda/laundrymanda.java
@@ -0,0 +1,9 @@
+package LaundryManda;
+public class laundrymanda {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        koneksi k = new koneksi();
+        k.connect();
+    }
+    
+}
--- src/apploundryukk/AppLoundryUKK.java
+++
@@ -1,9 +0,0 @@
-package apploundryukk;
-public class AppLoundryUKK {
-    public static void main(String[] args) {
-        // TODO code application logic here
-        koneksi k = new koneksi();
-        k.connect();
-    }
-    
-}
